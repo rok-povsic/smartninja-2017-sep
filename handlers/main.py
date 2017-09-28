f02a406f4909ca206b2dca8b28409d37407b9469
Rok Povšič <rok.povsic@gmail.com>
2017-09-28 19:24:03 +0200

    Added models. Added login/logout.

--- handlers/main.py
+++ handlers/main.py
@@ -1,8 +1,19 @@
+from google.appengine.api import users
+
 from handlers.base import BaseHandler
 
 
 class MainHandler(BaseHandler):
     def get(self):
+        user = users.get_current_user()
+
+        params = {}
+        if user:
+            params['user'] = user
+            params['logout_url'] = users.create_logout_url('/')
+        else:
+            params['login_url'] = users.create_login_url('/')
+
         return self.render_template(
-            "home.html")
+            "home.html", params)
 
