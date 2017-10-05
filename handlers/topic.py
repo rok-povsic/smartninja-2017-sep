925d2ffe4f8a4dec9a0020c9649283f696ff0430
Rok Povšič <rok.povsic@gmail.com>
2017-10-05 17:27:48 +0200

    Added making comments, showing comments.

--- handlers/topic.py
+++ handlers/topic.py
@@ -6,15 +6,17 @@ from google.appengine.api import memcache
 
 from handlers.base import BaseHandler
 from models.topic import Topic
+from models.comment import Comment
 
+def make_csrf_token():
+    csrf_token = str(uuid.uuid4())
+    memcache.add(csrf_token, True, time=600)
+    return csrf_token
 
 class AddTopicHandler(BaseHandler):
     def get(self):
-        csrf_token = str(uuid.uuid4())
-        memcache.add(csrf_token, True, time=600)
-
         params = {
-            "csrf_token": csrf_token
+            "csrf_token": make_csrf_token()
         }
         return self.render_template("add_topic.html",
                                     params)
@@ -32,4 +34,33 @@ class AddTopicHandler(BaseHandler):
         topic = Topic(title=title, content=text,
               user_email=email)
         topic.put()
+        return self.redirect('/')
+
+
+class TopicDetailsHandler(BaseHandler):
+    def get(self, topic_id):
+        topic = Topic.get_by_id(int(topic_id))
+        comments = Comment.query(Comment.topic_id == int(topic_id)).fetch()
+
+        params = {
+            "csrf_token": make_csrf_token(),
+            "topic": topic,
+            "comments": comments,
+        }
+        return self.render_template("topic_details.html",
+                                    params)
+
+    def post(self, topic_id):
+        csrf_token = self.request.get('csrf-token')
+        if not memcache.get(csrf_token):
+            return self.write("CSRF NAPAD")
+
+        text = cgi.escape(self.request.get('text'))
+
+        email = users.get_current_user().email()
+
+        topic = Topic.get_by_id(int(topic_id))
+
+        comment = Comment(content=text, user_email=email, topic_id=int(topic_id), topic_title=topic.title)
+        comment.put()
         return self.redirect('/')
--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import webapp2
 
 from handlers.main import MainHandler
 from handlers.cookie import CookieHandler
-from handlers.topic import AddTopicHandler
+from handlers.topic import AddTopicHandler, TopicDetailsHandler
 
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
     webapp2.Route('/set-cookie', CookieHandler),
     webapp2.Route('/add-topic', AddTopicHandler),
+    webapp2.Route('/topic-details/<topic_id:\d+>', TopicDetailsHandler),
 ], debug=True)
