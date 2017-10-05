77d88fba07d87ef5c17e5586c0b46d99755e6788
Rok Povšič <rok.povsic@gmail.com>
2017-10-05 16:07:40 +0200

    Display all topics.

--- handlers/main.py
+++ handlers/main.py
@@ -1,7 +1,9 @@
 from handlers.base import BaseHandler
+from models.topic import Topic
 
 
 class MainHandler(BaseHandler):
     def get(self):
-        return self.render_template("home.html")
+        topics = Topic.query(Topic.deleted == False).fetch()
+        return self.render_template("home.html", {"topics": topics})
 
