a6ad567a56eb32fc24a77e0725f6d2de4214b33e
Rok Povšič <rok.povsic@gmail.com>
2017-10-09 19:55:49 +0200

    Added 'fake' deleting topics. Added CRON to really delete old topics.

---
+++ crons/delete_topics_cron.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+
+from handlers.base import BaseHandler
+from models.topic import Topic
+from models.comment import Comment
+
+
+class DeleteTopicsCron(BaseHandler):
+    def get(self):
+        month_ago = datetime.now() - timedelta(days=30)
+        topics = Topic.query(Topic.deleted == True,
+                             Topic.created_at <= month_ago).fetch()
+        for topic in topics:
+            comments = Comment.query(
+                Comment.topic_id == topic.key.id()).fetch()
+            for comment in comments:
+                comment.key.delete()
+
+            topic.key.delete()
--- main.py
+++ main.py
@@ -3,14 +3,20 @@ import webapp2
 
 from handlers.main import MainHandler
 from handlers.cookie import CookieHandler
-from handlers.topic import AddTopicHandler, TopicDetailsHandler
+from handlers.topic import AddTopicHandler, TopicDetailsHandler, DeleteTopicHandler
 from workers.send_mail_worker import SendMailWorker
+from crons.delete_topics_cron import DeleteTopicsCron
 
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
     webapp2.Route('/set-cookie', CookieHandler),
+
     webapp2.Route('/add-topic', AddTopicHandler),
     webapp2.Route('/topic-details/<topic_id:\d+>', TopicDetailsHandler),
+    webapp2.Route('/topic-delete/<topic_id:\d+>', DeleteTopicHandler),
+
     webapp2.Route('/task/email-topic-author', SendMailWorker),
+
+    webapp2.Route('/cron/delete-topics', DeleteTopicsCron),
 ], debug=True)
